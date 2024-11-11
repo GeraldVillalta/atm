c19ff9f6b748964bebe5f4c4cd4072685ac46fc7
GeraldVillalta <96406688+GeraldVillalta@users.noreply.github.com>
2024-11-10 22:26:08 -0600

    inicio

---
+++ atm/src/test/java/poo/pp2/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package poo.pp2.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
